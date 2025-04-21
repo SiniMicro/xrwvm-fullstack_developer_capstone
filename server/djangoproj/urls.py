8c15b35323742c90c44de4c93a55db50c943b489
SiniMicro <161944287+SiniMicro@users.noreply.github.com>
2025-04-21 18:51:15 +0300

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from djangoapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="Home.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
+    path('logout/',view=views.logout_request, name ='logout')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
