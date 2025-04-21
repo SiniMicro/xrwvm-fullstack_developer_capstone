2bfc3f0a0048e70cc88f0f845b4d461d88a78ad0
SiniMicro <161944287+SiniMicro@users.noreply.github.com>
2025-04-21 19:03:16 +0300

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="Home.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
-    path('logout/',view=views.logout_request, name ='logout')
+    path('logout/',view=views.logout_request, name ='logout'),
+    path('register/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
