fadcdf6699e749367df39319b990824f05a60a70
Sini Sajin <SINI@MICROCENTERGULF.COM>
2025-04-23 09:27:02 -0400

    Added

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,15 +2,15 @@ import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import RegisterPanel from "./components/Register/Register"  //RegisterPanel es un nombre arbitrario dado al archivo de Register dentro de esta ruta.
 
-// import Dealers from './components/Dealers/Dealers'; 
-// import Dealer from "./components/Dealers/Dealer";
-// import PostReview from "./components/Dealers/PostReview"
+import Dealers from './components/Dealers/Dealers'; 
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview"
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<RegisterPanel />} />
-      {/* <Route path="/dealers" element={<Dealers />} />
+      <Route path="/dealers" element={<Dealers />} />
       <Route path="/dealer/:id" element={<Dealer />} />
       <Route path="/postreview/:id" element={<PostReview />} /> */}
     </Routes>
